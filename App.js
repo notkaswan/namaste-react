d382c72043a04b9f883dfc874b6df8c6095138ac
Deepak Kaswan <62922598+notkaswan@users.noreply.github.com>
2025-02-18 23:32:07 +0530

    first commit of project

---
+++ App.js
@@ -0,0 +1,13 @@
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "its heading 1."),
+    React.createElement("h2", {}, "its heading 2."),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "its heading 1."),
+    React.createElement("h2", {}, "its heading 2."),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
