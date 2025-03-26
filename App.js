f0e9ce22d93e459d3c6cdd75dbcd6b2380d05e90
Deepak Kaswan <62922598+notkaswan@users.noreply.github.com>
2025-03-27 03:55:17 +0530

    todays

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "its heading 1."),
@@ -10,4 +13,5 @@ const heading = React.createElement("div", { id: "parent" }, [
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(heading);
