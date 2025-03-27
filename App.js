f3391cd3c8608732d7c8d44cc984e1b818cf6e6b
Deepak Kaswan <62922598+notkaswan@users.noreply.github.com>
2025-03-27 07:00:27 +0530

    some changes

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "its heading 1."),
-    React.createElement("h2", {}, "its heading 2."),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "its heading 1."),
-    React.createElement("h2", {}, "its heading 2."),
-  ]),
-]);
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Namaste React 👌👌👌"
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
