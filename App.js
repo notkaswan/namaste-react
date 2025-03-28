0fb8f7f5be9a0387c71be24900ba308ea9e48418
Deepak Kaswan <62922598+notkaswan@users.noreply.github.com>
2025-03-29 02:40:45 +0530

    food delhivery

--- App.js
+++ App.js
@@ -1,23 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React element
-const Title = () => (
-  <h1 id="heading" className="heading">
-    Namaste React 👌 from jsx.
-  </h1>
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://media.istockphoto.com/id/1435983029/vector/food-delivery-logo-images.jpg?s=612x612&w=0&k=20&c=HXPxcjOxUiW4pMW1u9E0k2dJYQOU37a_0qZAy3so8fY="
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-// React component composition
-const HeadingComponent = () => (
-  <div id="container">
-    <Title />
-    <h1 id="heading" className="heading">
-      Namaste from Functional component 😊👌
-    </h1>
-  </div>
-);
+const Body = () => {
+  return <div className="body">body</div>;
+};
+
+const AppLayout = () => {
+  return (
+    <div>
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
