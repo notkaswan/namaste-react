cb42b2b214bfcbb530c825959431338bfc0fe174
Deepak Kaswan <62922598+notkaswan@users.noreply.github.com>
2025-04-07 18:37:00 +0530

    added top rated restaurant button

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,7 +3,16 @@ import resObj from "../utils/mockData";
 const Body = () => {
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            console.log("clicked filter");
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
       <div className="res-container">
         {resObj.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
