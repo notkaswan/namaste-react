7048d4cf0d0a8569af4074ef43b8d3e63b299fb5
Deepak Kaswan <62922598+notkaswan@users.noreply.github.com>
2025-04-16 14:43:22 +0530

    fetched data directly from swiggy api

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,10 +6,18 @@ const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState(resObj);
 
   useEffect(() => {
-    console.log("EFFECt rendered");
+    fetchData();
   }, []);
 
-  console.log("body rendered.");
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    console.log(json);
+  };
 
   return (
     <div className="body">
