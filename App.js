01b6732eedf4ec9f182a647f834aaf523d5fb505
Deepak Kaswan <62922598+notkaswan@users.noreply.github.com>
2025-05-12 03:25:37 +0530

    router added

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./src/components/Header";
 import Body from "./src/components/Body";
+import About from "./src/components/About";
+import Contact from "./src/components/Contact";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Error from "./src/components/Error";
 
 const AppLayout = () => {
   return (
@@ -12,6 +16,22 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/about",
+    element: <About />
+  },
+  {
+    path: "/contact",
+    element: <Contact />
+  }
+])
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About Us!</h1>
+            <h2>this is the about us page.</h2>
+        </div>
+    )
+}
+
+export default About;
