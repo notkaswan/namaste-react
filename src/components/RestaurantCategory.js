2eb74b68d9a1ec732b02a892c4145f2855316c70
Deepak Kaswan <62922598+notkaswan@users.noreply.github.com>
2025-06-10 07:58:26 +0530

    accrodion updated

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,26 +1,30 @@
 import { useState } from "react";
 import ItemList from "./ItemList";
 
-const RestaurantCategory = ({data}) => {
-    const [showItems, setShowItems] = useState(false)
-    const handleClick = () => {
-        setShowItems(!showItems)
-        
-    }
-    return(
-        <div>
-            {/* Accordion Header */}
+const RestaurantCategory = ({ data, showItems, setShowIndex }) => {
+  const handleClick = () => {
+    // setShowItems(!showItems)
+    setShowIndex();
+  };
+  return (
+    <div>
+      {/* Accordion Header */}
 
-            <div className="mx-auto my-4 w-1/2 bg-gray-200 p-4 shadow-xl">
-            <div className="flex justify-between cursor-pointer" onClick={handleClick}>
-                <span className="text-lg font-bold">{data.title} ({data.itemCards.length})</span>
-                <span>⬇️</span>
-            </div>
-            {/* Accordion Body */}
-            {showItems && <ItemList items= {data.itemCards} />}
-            </div>
+      <div className="mx-auto my-4 w-1/2 bg-gray-200 p-4 shadow-xl">
+        <div
+          className="flex justify-between cursor-pointer"
+          onClick={handleClick}
+        >
+          <span className="text-lg font-bold">
+            {data.title} ({data.itemCards.length})
+          </span>
+          <span>⬇️</span>
         </div>
-    )
-}
+        {/* Accordion Body */}
+        {showItems && <ItemList items={data.itemCards} />}
+      </div>
+    </div>
+  );
+};
 
-export default RestaurantCategory
+export default RestaurantCategory;
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -5,26 +5,42 @@ import useRestaurantMenu from "../utils/useRestaurantMenu";
 import RestaurantCategory from "./RestaurantCategory";
 
 const RestaurantMenu = () => {
-    // const [resInfo, setResInfo] = useState(null)
-    // const [resMenuInfo, setResMenuInfo] = useState(null);
-    const {resId} = useParams();
-    
-    const resData = useRestaurantMenu(resId);
+  // const [resInfo, setResInfo] = useState(null)
+  // const [resMenuInfo, setResMenuInfo] = useState(null);
+  const { resId } = useParams();
 
-    if (resData === null) return <Shimmer />;
-    const resInfo = resData?.cards[2]?.card?.card?.info
-    const resMenuInfo = resData?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards
-    const categories = resData?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(c=> c.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory")
-    return (
-        <div className="text-center">
-            <h1 className="font-bold text-2xl mt-10">{resInfo?.name}</h1>
-            <h2 className="italic">{resInfo?.areaName}, {resInfo?.city}</h2>
-            <h3 className="italic">{resInfo?.costForTwoMessage}</h3>
-            {categories.map((category) =>(
-                <RestaurantCategory key={category?.card?.card.title} data = {category?.card?.card}/>
-            ))}
-        </div>
-    )
-}
+  const resData = useRestaurantMenu(resId);
 
-export default RestaurantMenu
+  const [showIndex, setShowIndex] = useState(0);
+
+  if (resData === null) return <Shimmer />;
+  const resInfo = resData?.cards[2]?.card?.card?.info;
+  const resMenuInfo =
+    resData?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card
+      ?.itemCards;
+  const categories =
+    resData?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+      (c) =>
+        c.card?.card?.["@type"] ===
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+  return (
+    <div className="text-center">
+      <h1 className="font-bold text-2xl mt-10">{resInfo?.name}</h1>
+      <h2 className="italic">
+        {resInfo?.areaName}, {resInfo?.city}
+      </h2>
+      <h3 className="italic">{resInfo?.costForTwoMessage}</h3>
+      {categories.map((category, index) => (
+        <RestaurantCategory
+          key={category?.card?.card.title}
+          data={category?.card?.card}
+          showItems={index === showIndex ? true : false}
+          setShowIndex={() => setShowIndex(index)}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default RestaurantMenu;
